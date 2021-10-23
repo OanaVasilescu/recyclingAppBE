719d3666171e73d276b4a47a24a7a7b9324543de
OanaVasilescu <vasilescu.oana28@gmail.com>
2021-10-23 17:59:35 +0300

    Init

---
+++ src/main/java/recycle/greenlife/GreenlifeApplication.java
@@ -0,0 +1,13 @@
+package recycle.greenlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenlifeApplication.class, args);
+	}
+
+}
